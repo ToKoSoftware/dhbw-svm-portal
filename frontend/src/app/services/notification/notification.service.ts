5cca3c6bb929f763efd3e42cf1e7aee4a1904b79
Tomas Kostadinov <github@kostadinov.it>
2021-01-04 23:58:13 +0100

    * Support notification preseets

--- frontend/src/app/services/notification/notification.service.ts
+++ frontend/src/app/services/notification/notification.service.ts
@@ -7,7 +7,7 @@ import {BehaviorSubject} from "rxjs";
 export class NotificationService {
   public notifications$: BehaviorSubject<Notification[]> = new BehaviorSubject([]);
 
-  public createNotification(n: Notification, time: number | null = 3000) {
+  public createNotification(n: Notification, time: number | null = 3000): void {
     const currentNotifications = this.notifications$.getValue();
     currentNotifications.push(n);
     this.notifications$.next(currentNotifications);
@@ -16,10 +16,33 @@ export class NotificationService {
     }
   }
 
-  public removeNotification(id: string) {
+  public removeNotification(id: string): void {
     let currentNotifications = this.notifications$.getValue();
     currentNotifications = currentNotifications.filter(el => el.id !== id);
     this.notifications$.next(currentNotifications);
+  }
+
+  public savedSuccessfully(): void {
+    this.createNotification(
+      {
+        id: Math.random().toString(36).substring(7),
+        title: 'Speichern erfolgreich',
+        description: 'Ihre Änderungen wurden übernommen',
+        type: 'info'
+      }
+    );
+  }
+
+  public savingFailed(error = ''): void {
+    const description = error != '' ? error : 'Ihre Änderungen wurden nicht übernommen';
+    this.createNotification(
+      {
+        id: Math.random().toString(36).substring(7),
+        title: 'Speichern fehlgeschlagen',
+        description,
+        type: 'error'
+      }
+    );
   }
 
 }
@@ -31,4 +54,5 @@ export interface Notification {
   dismissible?: boolean;
   type: notificationType;
 }
-export type notificationType =  'info' | 'warning' | 'error';
+
+export type notificationType = 'info' | 'warning' | 'error';
--- frontend/src/app/ui/notifier/notifier.component.ts
+++ frontend/src/app/ui/notifier/notifier.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {NotificationService} from "../../services/notification/notification.service";
 
 @Component({
