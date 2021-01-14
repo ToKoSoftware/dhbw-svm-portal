56ac6e9473210def3f423d1b86b16680b988c04e
Tomas Kostadinov <github@kostadinov.it>
2021-01-14 17:05:13 +0100

    * Implement auto loading of current customer

--- server/src/vars.ts
+++ server/src/vars.ts
@@ -2,6 +2,7 @@ import Loggy from './functions/loggy.func';
 import {Configuration} from './interfaces/configutation.interface';
 import {Op, Sequelize} from 'sequelize';
 import { User } from './models/user.model';
+import {Organization} from './models/organization.model';
 
 
 export abstract class Vars {
@@ -10,5 +11,6 @@ export abstract class Vars {
     public static db: Sequelize;
     public static op: typeof Op;
     public static currentUser: User;
+    public static currentOrganization: Organization;
     public static currentJWT: string;
 }
