32f4bb341ab61f7d71375906049d4314708e8f86
Dominik Ruf <aquanik.ruf@gmail.com>
2021-01-14 19:45:47 +0100

    FEATURE: getTeams logic and Api routes

---
+++ server/src/api/v2/teams/get-teams.ts
@@ -0,0 +1,60 @@
+import { Request, Response } from 'express';
+import { FindOptions } from 'sequelize';
+import { buildQuery, QueryBuilderConfig } from '../../../functions/query-builder.func';
+import { currentUserIsAdminOrMatchesId } from '../../../functions/current-user-is-admin-or-matches-id.func';
+import { wrapResponse } from '../../../functions/response-wrapper';
+import { Team } from '../../../models/team.model';
+
+export async function getTeam(req: Request, res: Response): Promise<Response> {
+    let success = true;
+
+    if (!currentUserIsAdminOrMatchesId(req.params.id)) {
+        return res.status(403).send(wrapResponse(false, { error: 'Unauthorized!' }));
+    }
+
+    const data = await Team.findOne( //TODO: scope: currentOrg
+        {
+            where: {
+                id: req.params.id
+            }
+        })
+        .catch(() => {
+            success = false;
+            return null;
+        });
+    if (!success) {
+        return res.status(500).send(wrapResponse(false, { error: 'Database error' }));
+    }
+    if (data === null) {
+        return res.status(404).send(wrapResponse(false));
+    }
+    return res.send(wrapResponse(data != null, data));
+}
+
+export async function getTeams(req: Request, res: Response): Promise<Response> {
+    let query: FindOptions = {
+        raw: true,
+    };
+    const allowedSearchFilterAndOrderFields = ['title'];
+    const queryConfig: QueryBuilderConfig = {
+        query: query,
+        searchString: req.query.search as string || '',
+        allowLimitAndOffset: true,
+        allowedFilterFields: allowedSearchFilterAndOrderFields,
+        allowedSearchFields: allowedSearchFilterAndOrderFields,
+        allowedOrderFields: allowedSearchFilterAndOrderFields
+    };
+    query = buildQuery(queryConfig, req);
+
+    let success = true;
+    const data = await Team.findAll(query) //TODO: scope: currentOrg
+        .catch(() => {
+            success = false;
+            return null;
+        });
+    if (!success) {
+        return res.status(500).send(wrapResponse(false, { error: 'Database error' }));
+    }
+
+    return res.send(wrapResponse(true, data));
+}
