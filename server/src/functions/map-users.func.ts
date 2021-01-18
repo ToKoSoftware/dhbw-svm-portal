d96c5d41a9899703a8a3a78309e9764ebe11758c
Dominik Ruf <aquanik.ruf@gmail.com>
2021-01-18 16:34:09 +0100

    FIX: delete date parser

--- server/src/functions/map-users.func.ts
+++ server/src/functions/map-users.func.ts
@@ -5,11 +5,9 @@ export async function mapUser(incomingData: RawUserData): Promise<RawUserData> {
 
     const SALT_FACTOR = 10;
     const hashedPassword =  incomingData.password !== undefined ? await bcrypt.hash(incomingData.password, SALT_FACTOR) : incomingData.password;
-    const dateParts = incomingData.birthday !== undefined ? incomingData.birthday.toString().split('.') : incomingData.birthday;
-
+    
     return {
         ...incomingData,
-        birthday: incomingData.birthday !== undefined ? new Date(Date.UTC(+dateParts[2], +dateParts[1]-1, +dateParts[0])) : incomingData.birthday,
         password: hashedPassword,
         is_admin: false,
     };
