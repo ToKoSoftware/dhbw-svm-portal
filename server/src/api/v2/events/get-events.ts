ded4cb28dc7eba2c78433afe7202e07c85e357bc
LisaFremdt <lisa.fremdt@t-online.de>
2021-01-23 14:44:44 +0100

    added get-news, get-events, get-organizations, get-roles, get-polls and added routes to server.ts

---
+++ server/src/api/v2/events/get-events.ts
@@ -0,0 +1,67 @@
+import { Request, Response } from 'express';
+import { FindOptions} from 'sequelize';
+import { buildQuery, QueryBuilderConfig } from '../../../functions/query-builder.func';
+import { wrapResponse } from '../../../functions/response-wrapper';
+import { Event } from '../../../models/event.model';
+import { Vars } from '../../../vars';
+import { EventData } from '../../../interfaces/event.interface';
+import { User } from '../../../models/user.model';
+import { Organization } from '../../../models/organization.model';
+
+
+export async function getEvent(req: Request, res: Response): Promise<Response> {
+    let success = true;
+
+
+    const eventData: EventData | null = await Event
+        .scope({method: ['onlyCurrentOrg', Vars.currentOrganization.id]})
+        .findOne({
+            where: {
+                    id: req.params.id   
+            },
+            ... Vars.currentUser.is_admin ? {
+                include: [Organization, {model: User, as: 'author'}, {model: User, as: 'registered_users'}]
+            } : {
+                include: {
+                    model: User.scope('publicData')
+                }
+            }
+        })
+        .catch(() => {
+            success = false;
+            return null;
+        });
+    if (!success) {
+        return res.status(500).send(wrapResponse(false, { error: 'Database error' }));
+    }
+    if (eventData === null) {
+        return res.status(404).send(wrapResponse(false));
+    }
+    return res.send(wrapResponse(true, eventData));
+}
+
+export async function getEvents(req: Request, res: Response): Promise<Response> {
+    let query: FindOptions = {};
+    const allowedSearchFilterAndOrderFields = ['title','date'];
+    const queryConfig: QueryBuilderConfig = {
+        query: query,
+        searchString: req.query.search as string || '',
+        allowLimitAndOffset: true,
+        allowedFilterFields: allowedSearchFilterAndOrderFields,
+        allowedSearchFields: allowedSearchFilterAndOrderFields,
+        allowedOrderFields: allowedSearchFilterAndOrderFields
+    };
+    query = buildQuery(queryConfig, req);
+
+    let success = true;
+    const data = await Event.scope(['full', {method: ['onlyCurrentOrg', Vars.currentOrganization.id]}]).findAll(query)
+        .catch(() => {
+            success = false;
+            return null;
+        });
+    if (!success) {
+        return res.status(500).send(wrapResponse(false, { error: 'Database error' }));
+    }
+
+    return res.send(wrapResponse(true, data));
+}
---
+++ server/src/api/v2/news/get-news.ts
@@ -0,0 +1,67 @@
+import { Request, Response } from 'express';
+import { FindOptions} from 'sequelize';
+import { buildQuery, QueryBuilderConfig } from '../../../functions/query-builder.func';
+import { wrapResponse } from '../../../functions/response-wrapper';
+import { News } from '../../../models/news.model';
+import { Vars } from '../../../vars';
+import { NewsData } from '../../../interfaces/news.interface';
+import { User } from '../../../models/user.model';
+import { Organization } from '../../../models/organization.model';
+
+
+export async function getNews(req: Request, res: Response): Promise<Response> {
+    let success = true;
+
+
+    const newsData: NewsData | null = await News
+        .scope({method: ['onlyCurrentOrg', Vars.currentOrganization.id]})
+        .findOne({
+            where: {
+                    id: req.params.id   
+            },
+            ... Vars.currentUser.is_admin ? {
+                include: [Organization, User]
+            } : {
+                include: {
+                    model: User.scope('publicData')
+                }
+            }
+        })
+        .catch(() => {
+            success = false;
+            return null;
+        });
+    if (!success) {
+        return res.status(500).send(wrapResponse(false, { error: 'Database error' }));
+    }
+    if (newsData === null) {
+        return res.status(404).send(wrapResponse(false));
+    }
+    return res.send(wrapResponse(true, newsData));
+}
+
+export async function getAllNews(req: Request, res: Response): Promise<Response> {
+    let query: FindOptions = {};
+    const allowedSearchFilterAndOrderFields = ['title'];
+    const queryConfig: QueryBuilderConfig = {
+        query: query,
+        searchString: req.query.search as string || '',
+        allowLimitAndOffset: true,
+        allowedFilterFields: allowedSearchFilterAndOrderFields,
+        allowedSearchFields: allowedSearchFilterAndOrderFields,
+        allowedOrderFields: allowedSearchFilterAndOrderFields
+    };
+    query = buildQuery(queryConfig, req);
+
+    let success = true;
+    const data = await News.scope(['full', {method: ['onlyCurrentOrg', Vars.currentOrganization.id]}]).findAll(query)
+        .catch(() => {
+            success = false;
+            return null;
+        });
+    if (!success) {
+        return res.status(500).send(wrapResponse(false, { error: 'Database error' }));
+    }
+
+    return res.send(wrapResponse(true, data));
+}
