f056502604b8c336bb6e0cca30973cabf37872cb
Dominik Ruf <aquanik.ruf@gmail.com>
2021-02-02 23:59:20 +0100

    FEATURE: begin implementation of update-event

--- server/src/api/v2/events/update-event.ts
+++ server/src/api/v2/events/update-event.ts
@@ -1,8 +1,29 @@
 import { Request, Response } from 'express';
 import { wrapResponse } from '../../../functions/response-wrapper';
+import { RawEventData } from '../../../interfaces/event.interface';
+import { Event } from '../../../models/event.model';
 
 export async function updateEvent(req: Request, res: Response): Promise<Response> {
-    
+    let success = true;
+    const incomingData = req.body;
+    const eventId = req.params.id;
+
+    const eventData: RawEventData | null = await Event.findOne(
+        {
+            where: {
+                id: eventId
+            }
+        })
+        .catch(() => {
+            success = false;
+            return null;
+        });
+    if (!success) {
+        return res.status(500).send(wrapResponse(false, { error: 'Database error' }));
+    }
+    if (eventData === null) {
+        return res.status(400).send(wrapResponse(false, { error: 'No Event with given id found' }));
+    }
 
     return res.send(wrapResponse(true, {}));
 }
