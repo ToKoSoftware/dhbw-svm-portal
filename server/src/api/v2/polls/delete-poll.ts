a316637e2775f872817fc9fec285aa720f39dbc9
Dominik Ruf <aquanik.ruf@gmail.com>
2021-02-06 12:32:12 +0100

    FIX: Delete poll

--- server/src/api/v2/polls/delete-poll.ts
+++ server/src/api/v2/polls/delete-poll.ts
@@ -1,12 +1,26 @@
-import {Request, Response} from 'express';
-import {wrapResponse} from '../../../functions/response-wrapper';
-import {Poll} from '../../../models/poll.model';
-import {PollAnswer} from '../../../models/poll-answer.model';
+import { Request, Response } from 'express';
+import { wrapResponse } from '../../../functions/response-wrapper';
+import { Poll } from '../../../models/poll.model';
+import { PollAnswer } from '../../../models/poll-answer.model';
 
 export async function deletePoll(req: Request, res: Response): Promise<Response> {
     let success = true;
     //TODO Authoriaztion check, if polls can get created by no-admins
-    await Poll.update(
+    const pollId: string = req.params.id;
+
+    const pollData: Poll | null = await Poll.scope('active').findByPk(pollId)
+        .catch(() => {
+            success = false;
+            return null;
+        });
+    if (!success) {
+        return res.status(500).send(wrapResponse(false, { error: 'Database error' }));
+    }
+    if (pollData === null) {
+        return res.status(400).send(wrapResponse(false, { error: 'No poll with given id found!' }));
+    }
+
+    await pollData.update(
         {
             is_active: false,
         },
@@ -21,7 +35,7 @@ export async function deletePoll(req: Request, res: Response): Promise<Response>
             return null;
         });
     if (!success) {
-        return res.status(500).send(wrapResponse(false, {error: 'Could not deactivate poll with id ' + req.params.id}));
+        return res.status(500).send(wrapResponse(false, { error: 'Could not deactivate poll with id ' + pollId }));
     }
 
     await PollAnswer.update(
@@ -30,7 +44,7 @@ export async function deletePoll(req: Request, res: Response): Promise<Response>
         },
         {
             where: {
-                poll_id: req.params.id,
+                poll_id: pollId,
                 is_active: true
             }
         })
@@ -39,10 +53,10 @@ export async function deletePoll(req: Request, res: Response): Promise<Response>
             return null;
         });
     if (!success) {
-        return res.status(500).send(wrapResponse(false, {error: 'Could not deactivate pollanswer with id ' + req.params.id}));
+        return res.status(500).send(wrapResponse(false, { error: 'Could not deactivate pollanswer belonging to poll with id ' + pollId }));
     }
 
     return res.status(204).send(wrapResponse(true));
-    
-    
+
+
 }
