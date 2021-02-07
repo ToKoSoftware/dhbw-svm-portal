337230ed66aad304b22e5ac9258264f0f2ac6e09
Dominik Ruf <aquanik.ruf@gmail.com>
2021-02-07 13:24:05 +0100

    REFACTOR: delete membership

--- server/src/api/v2/teams/delete-membership.ts
+++ server/src/api/v2/teams/delete-membership.ts
@@ -3,24 +3,26 @@ import { wrapResponse } from '../../../functions/response-wrapper';
 import { currentUserIsAdminOrMatchesId } from '../../../functions/current-user-is-admin-or-matches-id.func';
 import { Vars } from '../../../vars';
 import { Membership } from '../../../models/membership.model';
+import { RawMembershipData } from '../../../interfaces/membership.interface';
 
 export async function deleteMembership(req: Request, res: Response): Promise<Response> {
     let success = true;
+    const incomingData: RawMembershipData = req.body;
 
     //check if currentUser is admin oder member
-    const memberhsipToDelete = await Membership.findByPk(req.params.id)
-        .catch(() => {
-            success = false;
-            return null;
-        });
-
+    const memberhsipToDelete = await Membership.findOne({
+        where: {
+            user_id: incomingData.user_id,
+            team_id: req.params.team_id
+        }
+    });
 
     if (!success) {
         return res.status(500).send(wrapResponse(false, { error: 'Database error' }));
     }
 
     if (memberhsipToDelete === null) {
-        return res.status(404).send(wrapResponse(false, { error: 'No membership with given id' }));
+        return res.status(404).send(wrapResponse(false, { error: 'No membership with given ids' }));
     } else if (memberhsipToDelete.user_id !== null && !currentUserIsAdminOrMatchesId(memberhsipToDelete.user_id) && !Vars.currentUser.is_admin) {
         return res.status(403).send(wrapResponse(false, { error: 'Unauthorized!' }));
     }
