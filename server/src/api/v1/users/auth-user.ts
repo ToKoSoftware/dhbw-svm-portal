c9c7d739f79ddec26f12c7a4a3c54f6954679c57
Dominik Ruf <aquanik.ruf@gmail.com>
2021-02-08 09:58:19 +0100

    FIX: linting

--- server/src/api/v1/users/auth-user.ts
+++ server/src/api/v1/users/auth-user.ts
@@ -1,9 +1,9 @@
-import {Request, Response} from 'express';
-import {wrapResponse} from '../../../functions/response-wrapper';
-import {UserLoginData} from '../../../interfaces/users.interface';
-import {User} from '../../../models/user.model';
+import { Request, Response } from 'express';
+import { wrapResponse } from '../../../functions/response-wrapper';
+import { UserLoginData } from '../../../interfaces/users.interface';
+import { User } from '../../../models/user.model';
 import * as bcrypt from 'bcryptjs';
-import {jwtSign} from '../../../functions/jwt-sign.func';
+import { jwtSign } from '../../../functions/jwt-sign.func';
 import { checkKeysAreNotEmptyOrNotSet } from '../../../functions/check-inputs.func';
 import { Vars } from '../../../vars';
 import { userIsAdminCheck } from '../../../functions/user-is-admin-check.func';
@@ -12,7 +12,7 @@ export async function loginUser(req: Request, res: Response): Promise<Response> 
 
     const incomingData: UserLoginData = req.body;
 
-    if (!checkKeysAreNotEmptyOrNotSet(incomingData, ['email', 'password']) || !checkKeysAreNotEmptyOrNotSet(incomingData, ['user', 'password'])){
+    if (!checkKeysAreNotEmptyOrNotSet(incomingData, ['email', 'password']) || !checkKeysAreNotEmptyOrNotSet(incomingData, ['user', 'password'])) {
         return res.status(400).send(wrapResponse(false, { error: 'Not all required fields have been set' }));
     }
 
@@ -20,11 +20,11 @@ export async function loginUser(req: Request, res: Response): Promise<Response> 
     const selectStatement = {
         where: {
             is_active: true,
-            ...(incomingData?.email ? {email: incomingData.email} : {}),
-            ...(incomingData?.username ? {email: incomingData.username} : {}),
+            ...(incomingData?.email ? { email: incomingData.email } : {}),
+            ...(incomingData?.username ? { email: incomingData.username } : {}),
         }
     };
-    
+
     const user = await User.scope('verification').findOne(selectStatement)
         .catch((error) => {
             Vars.loggy.log(error);
@@ -34,10 +34,10 @@ export async function loginUser(req: Request, res: Response): Promise<Response> 
 
 
     if (!success) {
-        return res.status(500).send(wrapResponse(false, {error: 'Database error'}));
+        return res.status(500).send(wrapResponse(false, { error: 'Database error' }));
     }
     if (user === null) {
-        return res.status(403).send(wrapResponse(false, {error: 'Unauthorized'}));
+        return res.status(403).send(wrapResponse(false, { error: 'Unauthorized' }));
     } else {
         const passwordMatches = await bcrypt.compare(incomingData.password, user.password)
             .catch(() => false);
@@ -47,7 +47,7 @@ export async function loginUser(req: Request, res: Response): Promise<Response> 
             const token = jwtSign(user);
             return res.send(wrapResponse(true, token));
         }
-        return res.status(403).send(wrapResponse(false, {error: 'Unauthorized!'}));
+        return res.status(403).send(wrapResponse(false, { error: 'Unauthorized!' }));
     }
 }
 
