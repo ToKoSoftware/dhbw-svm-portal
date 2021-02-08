7668c96b678f4d64f928ed3aac3bb2648ac76dd9
Dominik Ruf <aquanik.ruf@gmail.com>
2021-02-08 13:35:38 +0100

    FIX: validators on event and poll

--- server/src/functions/map-events.func.ts
+++ server/src/functions/map-events.func.ts
@@ -4,6 +4,8 @@ import { Vars } from '../vars';
 export function mapEvent(incomingData: EventDataSnapshot): RawEventData {
     return {
         ...incomingData,
+        start_date: new Date(incomingData.start_date),
+        end_date: new Date(incomingData.end_date),
         author_id: Vars.currentUser.id,
         org_id: Vars.currentOrganization.id
     };
