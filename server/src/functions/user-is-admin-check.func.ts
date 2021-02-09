fdc0b320c381f522e5598a5af180354958ba192e
Dominik Ruf <aquanik.ruf@gmail.com>
2021-02-09 17:00:58 +0100

    FIX: userIsAdminCheck when org_id is null

--- server/src/functions/user-is-admin-check.func.ts
+++ server/src/functions/user-is-admin-check.func.ts
@@ -1,5 +1,10 @@
 import { User } from '../models/user.model';
+import { Vars } from '../vars';
 
 export function userIsAdminCheck(user: User): boolean {
+    Vars.loggy.log(user);
+    if (user.org_id === null) {
+        return false;
+    }
     return user.assigned_roles.find(el => el.id === user.organization.admin_role_id) !== undefined;
 }
