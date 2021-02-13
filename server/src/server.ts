b96bc0b849c997c5e87289dc0f2cfa2ac6ec0004
Dominik Ruf <aquanik.ruf@gmail.com>
2021-02-13 13:19:09 +0100

    FEATURE: deleteDirectDebitMandate

--- server/src/server.ts
+++ server/src/server.ts
@@ -58,6 +58,8 @@ import { updateEventRegistration } from './api/v2/event-registrations/update-eve
 import { deleteOrganization } from './api/v2/organizations/delete-organization';
 import { updatePollVote } from './api/v2/poll-votes/update-poll-vote';
 import { createDirectDebitMandate } from './api/v2/direct-debit-mandate/create-direct-debit-mandate';
+import { getDirectDebitMandate } from './api/v2/direct-debit-mandate/get-direct-debit-mandate';
+import { deleteDirectDebitMandate } from './api/v2/direct-debit-mandate/delete-direct-debit-mandate';
 
 export default function startServer(): void {
 
@@ -190,7 +192,7 @@ export default function startServer(): void {
      */
     app.get('/api/v2/directDebitMandate', userIsAuthorized, (req, res) => getDirectDebitMandate(req, res));
     app.post('/api/v2/directDebitMandate', userIsAuthorized, (req, res) => createDirectDebitMandate(req, res));
-    //app.delete('/api/v2/directDebitMandate', userIsAuthorized, (req, res) => deleteDirectDebitMandate(req, res));
+    app.delete('/api/v2/directDebitMandate', userIsAuthorized, (req, res) => deleteDirectDebitMandate(req, res));
 
     /**
      * Admin
