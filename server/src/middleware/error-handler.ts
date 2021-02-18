9ebbb1208ad778da6382dbebb8df3cade6f94043
BlueEnni <Enricobachus@t-online.de>
2021-02-18 21:02:33 +0100

    adjusted err.stack > depend on Vars.config.logging

--- server/src/middleware/error-handler.ts
+++ server/src/middleware/error-handler.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 import { PortalErrors } from '../enum/errors';
+import { wrapResponse } from '../functions/response-wrapper';
 import { PortalErrorData } from '../interfaces/error-handler-interface';
+import { Vars } from '../vars';
 
 
 export class customError extends Error {
@@ -9,15 +11,21 @@ export class customError extends Error {
     }
 }
 
+type IErrorHandler = (err: customError, req: Request, res: Response, next: NextFunction) => unknown;
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function errorHandler(err: customError, req: Request, res: Response, next: NextFunction): void {
+export const errorHandler: IErrorHandler = (err, req, res, next) => {
     const error: PortalErrorData = {
         message: err.errorEnumMessage.toString() || 'Es ist ein Fehler aufgetreten',
         status: err.statusCode || 500,
         type: PortalErrors[ err.errorEnumMessage ].toString(),
-        stacktrace: err.stack?.split('\n    '),
         success: false,
     };
 
-    res.status(error.status).send(error);
-}
+    if (Vars.config.logging) error.stacktrace = err.stack?.split('\n    ');
+
+    res.status(error.status).send(wrapResponse(false, error));
+};
+
+
+
