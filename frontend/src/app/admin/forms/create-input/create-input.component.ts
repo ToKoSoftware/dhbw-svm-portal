785900a61226514b594b3563534d33e56a398008
Tomas Kostadinov <github@kostadinov.it>
2021-03-05 14:27:20 +0100

    * Create shop and forms admin pages

---
+++ frontend/src/app/admin/forms/create-input/create-input.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-input',
+  templateUrl: './create-input.component.html'
+})
+export class CreateInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
