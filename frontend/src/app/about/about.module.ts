f4821784c02a17768cf9943c7d69df726e3587ea
Tomas Kostadinov <github@kostadinov.it>
2021-03-05 22:01:27 +0100

    * Created about/contact page

--- frontend/src/app/about/about.module.ts
+++ frontend/src/app/about/about.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AboutComponent } from './about/about.component';
+import {UiModule} from '../ui/ui.module';
+import {aboutRoutes} from './about.routes';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AboutComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    UiModule,
+    aboutRoutes
   ]
 })
 export class AboutModule { }
