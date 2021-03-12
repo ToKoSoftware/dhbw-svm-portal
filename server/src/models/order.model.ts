d9ed84f25daeb3b5b329e89df4c451279835a029
Dominik Ruf <aquanik.ruf@gmail.com>
2021-03-12 11:31:43 +0100

    FIX: Add currentorg scope to order FEATURE: Add files for API functions

--- server/src/models/order.model.ts
+++ server/src/models/order.model.ts
@@ -1,6 +1,7 @@
 import { Table, PrimaryKey, Column, IsInt, ForeignKey, BeforeCreate, BelongsTo, Scopes, DefaultScope } from 'sequelize-typescript';
 import { v4 as uuidv4 } from 'uuid';
 import { RawOrderData } from '../interfaces/order.interface';
+import { currentOrg } from './current-org.scope';
 import { Item } from './item.model';
 import { LoggedModel } from './logged.model';
 import { Organization } from './organization.model';
@@ -16,6 +17,7 @@ import { User } from './user.model';
         required: false,
         include: [Organization]
     },
+    onlyCurrentOrg: (org_id: string) => currentOrg(org_id),
     ordered: {
         required: false,
         order: [['createdAt', 'DESC']]
